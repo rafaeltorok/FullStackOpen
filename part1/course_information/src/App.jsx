3b1abb2eac3007346fbc974b7045180d3e5bccc2
Rafael Torok <rafaeltorok@gmail.com>
2025-10-15 18:23:15 -0300

    Completed exercises 1.3–1.5 from Part 1-b

--- part1/course_information/src/App.jsx
+++ part1/course_information/src/App.jsx
@@ -3,33 +3,35 @@ import Content from "./components/Content";
 import Total from "./components/Total";
 
 
-// Generates a unique key to use in each Part component, inside Content
-const generateKey = (course, part) => {
-  const key = course.toLocaleLowerCase().replace(' ', '-') + 
-              '-' + part.toLocaleLowerCase().replace(' ', '-');
-  return key;
-}
-
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
 
   return (
     <div>
-      <Header course={course}/>
+      <Header course={course.name}/>
       <Content
-        parts={[
-          [part1, exercises1, generateKey(course, part1)],
-          [part2, exercises2, generateKey(course, part2)],
-          [part3, exercises3, generateKey(course, part3)]
-        ]}
+        course={course.name}
+        parts={course.parts}
       />
-      <Total exercises={exercises1 + exercises2 + exercises3}/>
+      <Total exercises={totalExercises}/>
     </div>
   );
 }
--- part1/course_information/src/components/Content.jsx
+++ part1/course_information/src/components/Content.jsx
@@ -1,13 +1,20 @@
 import Part from './Part';
 
-export default function Content({ parts }) {
+export default function Content({ course, parts }) {
+  // Generates a unique key for each Part component
+  const generateKey = (course, name) => {
+    const key = (course + '-' + name).toLocaleLowerCase().replaceAll(' ', '-');
+    // console.log(key); // Debug only
+    return key;
+  }
+
   return (
     <div>
-      {parts.map(([part, exercises, key]) => (
+      {parts.map(part => (
         <Part
-          key={key}
-          part={part}
-          exercises={exercises}
+          key={generateKey(course, part.name)}
+          part={part.name}
+          exercises={part.exercises}
         />
       ))}
     </div>

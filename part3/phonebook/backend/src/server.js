ffb01263314f57619a69930d13a21b162ca03872
Rafael Torok <rafaeltorok@gmail.com>
2025-10-20 20:45:59 -0300

    General improvements to the Phonebook app from Part 3 Added support for serving static files through the backend Configured a proxy in the vite config file for the frontend

--- part3/phonebook/backend/src/server.js
+++ part3/phonebook/backend/src/server.js
@@ -11,6 +11,7 @@ morgan.token('body', (req) => {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('./dist'));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use('/', phonebookRouter);
 

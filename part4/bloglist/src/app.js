01de6fa3463b77517c9e7f17c14bc04ca72470aa
rafaeltorok <rafaeltorok@gmail.com>
2025-11-06 12:26:24 -0300

    Implement token authentication support on BlogsList

--- part4/bloglist/src/app.js
+++ part4/bloglist/src/app.js
@@ -4,6 +4,7 @@ const blogsRouter = require('./controllers/blogs.js')
 const usersRouter = require('./controllers/users.js')
 const config = require('./utils/config.js')
 const { errorHandler } = require('./utils/middleware.js')
+const loginRouter = require('./controllers/login.js')
 
 const app = express()
 
@@ -12,6 +13,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(errorHandler)
 
 module.exports = app

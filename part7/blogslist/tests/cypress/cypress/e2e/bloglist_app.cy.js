6d802d4dd495c41994a1084c572f6b9af72a0916
rafaeltorok <rafaeltorok@gmail.com>
2026-01-05 17:36:56 -0300

    Refactored the Cypress E2E tests The tests should now work properly with the new updated UI from the View exercises

--- part7/blogslist/tests/cypress/cypress/e2e/bloglist_app.cy.js
+++ part7/blogslist/tests/cypress/cypress/e2e/bloglist_app.cy.js
@@ -57,24 +57,16 @@ describe('Blogs List app', function() {
     })
 
     it('Only the user who created a blog can see the delete button', function() {
-      cy.contains('.blog-title', 'My test blog by Cypress')
-        .closest('table')
-        .within(() => {
-          cy.contains('button', 'show').click()
-          cy.contains('button', 'delete').should('be.visible')
-        })
+      cy.contains('li', 'My test blog by Cypress').click()
+      cy.contains('button', 'delete').should('be.visible')
 
       cy.contains('button', 'logout').click()
       
       cy.createUser({ username: 'test', name: 'Test user', password: 'test' })
       cy.login({ username: 'test', password: 'test' })
 
-      cy.contains('.blog-title', 'My test blog by Cypress')
-        .closest('table')
-        .within(() => {
-          cy.contains('button', 'show').click()
-          cy.contains('button', 'delete').should('not.exist')
-        })
+      cy.contains('li', 'My test blog by Cypress').click()
+      cy.contains('button', 'delete').should('not.exist')
     })
   })
 
@@ -89,45 +81,37 @@ describe('Blogs List app', function() {
     })
 
     it('The blog details can be shown', function () {
+      cy.contains('li', 'My test blog by Cypress').click()
+
       cy.contains('.blog-title', 'My test blog by Cypress')
-        .closest('table')
-        .within(() => {
-          cy.contains('button', 'show').click()
-          
-          cy.contains('th', 'URL:').next().should('contain', 'https://testing-with-cypress.com')
-          cy.contains('th', 'Likes:').next().should('contain', '0')
-          cy.contains('th', 'User:').next().should('contain', 'The Administrator')
-        })
+      cy.contains('th', 'URL:').next().should('contain', 'https://testing-with-cypress.com')
+      cy.contains('th', 'Likes:').next().should('contain', '0')
+      cy.contains('th', 'Added by:').next().should('contain', 'The Administrator')
     })
 
     it('Users can like a blog', function() {
-      cy.contains('.blog-title', 'My test blog by Cypress')
-        .closest('table')
-        .within(() => {
-          cy.contains('button', 'show').click()
+      cy.contains('li', 'My test blog by Cypress').click()
 
-          cy.contains('th', 'Likes:')
-            .next()
-            .contains('button', 'like')
-            .click()
+      cy.contains('th', 'Likes:')
+        .next()
+        .contains('button', 'like')
+        .click()
             
-          cy.get('.like-count').should('contain', '1')
-        })
+      cy.get('.like-count').should('contain', '1')
     })
 
     it('A blog can be removed', function() {
-      cy.contains('.blog-title', 'My test blog by Cypress')
-        .closest('table')
-        .within(() => {
-          cy.contains('button', 'show').click()
-          cy.on('window:confirm', (text) => {
-            expect(text).to.contains('Are you sure you want to remove the blog "My test blog" by Cypress from the list?')
-            return true // Accept
-          })
-          cy.contains('button', 'delete').click()
-        })
+      cy.contains('li', 'My test blog by Cypress').click()
+      
+      cy.on('window:confirm', (text) => {
+        expect(text).to.contains('Are you sure you want to remove the blog "My test blog" by Cypress from the list?')
+        return true // Accept
+      })
+      cy.contains('button', 'delete').click()
 
-      cy.contains('.blog-title', 'My test blog by Cypress').should('not.exist')
+      cy.get('.success-message')
+        .should('contain', 'The blog "My test blog" by Cypress was removed from the list')
+      cy.contains('li', 'My test blog by Cypress').should('not.exist')
     })
   })
 
@@ -153,42 +137,39 @@ describe('Blogs List app', function() {
 
     it('Blogs are ordered by the number of likes', function() {
       // Like "Another blog" 3 times
-      cy.contains('.blog-title', 'Another blog by Cypress')
-        .closest('table')
-        .as('anotherBlog')
+      cy.contains('li', 'Another blog by Cypress').click()
       
-      cy.get('@anotherBlog').contains('button', 'show').click()
-      cy.get('@anotherBlog').contains('button', 'like').click()
-      cy.get('@anotherBlog').contains('.like-count', '1')
-      cy.get('@anotherBlog').contains('button', 'like').click()
-      cy.get('@anotherBlog').contains('.like-count', '2')
-      cy.get('@anotherBlog').contains('button', 'like').click()
-      cy.get('@anotherBlog').contains('.like-count', '3')
+      cy.contains('button', 'like').click()
+      cy.contains('.like-count', '1')
+      cy.contains('button', 'like').click()
+      cy.contains('.like-count', '2')
+      cy.contains('button', 'like').click()
+      cy.contains('.like-count', '3')
+
+      cy.contains('a', 'home').click()
 
       // Like "One more blog" 2 times
-      cy.contains('.blog-title', 'One more blog by Cypress')
-        .closest('table')
-        .as('oneMoreBlog')
+      cy.contains('li', 'One more blog by Cypress').click()
       
-      cy.get('@oneMoreBlog').contains('button', 'show').click()
-      cy.get('@oneMoreBlog').contains('button', 'like').click()
-      cy.get('@oneMoreBlog').contains('.like-count', '1')
-      cy.get('@oneMoreBlog').contains('button', 'like').click()
-      cy.get('@oneMoreBlog').contains('.like-count', '2')
+      cy.contains('button', 'like').click()
+      cy.contains('.like-count', '1')
+      cy.contains('button', 'like').click()
+      cy.contains('.like-count', '2')
+
+      cy.contains('a', 'home').click()
 
       // Like "New blog" 1 time
-      cy.contains('.blog-title', 'New blog by Cypress')
-        .closest('table')
-        .as('newBlog')
+      cy.contains('li', 'New blog by Cypress').click()
       
-      cy.get('@newBlog').contains('button', 'show').click()
-      cy.get('@newBlog').contains('button', 'like').click()
-      cy.get('@newBlog').contains('.like-count', '1')
+      cy.contains('button', 'like').click()
+      cy.contains('.like-count', '1')
+
+      cy.contains('a', 'home').click()
 
       // Check if the order is correct
-      cy.get('.blog').eq(0).should('contain', 'Another blog')
-      cy.get('.blog').eq(1).should('contain', 'One more blog')
-      cy.get('.blog').eq(2).should('contain', 'New blog')
+      cy.get('.blogs-list li').eq(0).should('contain', 'Another blog')
+      cy.get('.blogs-list li').eq(1).should('contain', 'One more blog')
+      cy.get('.blogs-list li').eq(2).should('contain', 'New blog')
     })
   })
 })

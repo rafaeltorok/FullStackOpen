4ebe4d086407dc77557bd4d036329846913680a6
Rafael Torok <rafaeltorok@gmail.com>
2026-03-16 10:06:42 -0300

    Added new type for Entry on the Patientor app

--- part9/patientor/server/src/types.ts
+++ part9/patientor/server/src/types.ts
@@ -1,7 +1,15 @@
 import { NewPatientSchema } from "./schemas/newPatient";
 import z from "zod";
 
-export type Diagnosis = {
+export interface Entry {
+  id: string;
+  description: string;
+  date: Date;
+  specialist: string;
+  diagnosisCodes?: Diagnosis[];
+}
+
+export interface Diagnosis {
   code: string;
   name: string;
   latin?: string;
@@ -15,13 +23,16 @@ export enum Gender {
 
 export type NewPatientEntry = z.infer<typeof NewPatientSchema>;
 
-export type Patient = {
+export interface Patient {
   id: string;
   name: string;
   dateOfBirth: string;
   ssn: string;
   gender: Gender;
   occupation: string;
+  entries: Entry[];
 };
+
+export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 
 export type PatientInfo = Omit<Patient, "ssn">;

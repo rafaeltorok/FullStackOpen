01260fc27cc9c6b088e427c0f19f11f0a374825d
Rafael Torok <rafaeltorok@gmail.com>
2026-03-26 08:37:19 -0300

    Improved the error handling middleware - Updated the middleware to prevent the server from crashing during a 500 status code error

--- part9/patientor/server/src/middleware/errorHandler.ts
+++ part9/patientor/server/src/middleware/errorHandler.ts
@@ -5,11 +5,15 @@ export default function errorMiddleware(
   error: unknown,
   _req: Request,
   res: Response,
-  next: NextFunction,
+  _next: NextFunction,
 ) {
   if (error instanceof z.ZodError) {
     return res.status(400).json({ error: error.issues });
-  } else {
-    return next(error);
   }
+
+  console.error(error);
+
+  return res.status(500).json({
+    error: "Internal server error"
+  });
 }

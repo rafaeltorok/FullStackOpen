fe3de34ac92a7d4df79280d3008539fc4092d7e9
Rafael Torok <rafaeltorok@gmail.com>
2026-03-26 08:46:47 -0300

    Improved the Entry parser to use Zod - Removed the switch type statement along with the TypeScript exhaustive check - Fixed the server sending a 500 status code during an entry type check error, now Zod handles it with a proper 400 status code

--- part9/patientor/server/src/middleware/newEntryParser.ts
+++ part9/patientor/server/src/middleware/newEntryParser.ts
@@ -1,32 +1,29 @@
-import { NewEntry } from "../../../shared/types";
-import { NewHealthCheckSchema } from "../schemas/newHealthCheckEntry";
+// Zod dependencies
+import z from 'zod';
+
+// Entry schemas
 import { NewHospitalSchema } from "../schemas/newHospitalEntry";
+import { NewHealthCheckSchema } from "../schemas/newHealthCheckEntry";
 import { NewOccupationalHealthcareSchema } from "../schemas/newOccupationalEntry";
+
+// TypeScript types
 import type { Request, Response, NextFunction } from "express";
+import { NewEntry } from "../../../shared/types";
 
 export default function newEntryParser(
   req: Request<{ id: string }, unknown, NewEntry>,
   _res: Response,
   next: NextFunction,
 ) {
-  function exhaustiveMatchingGuard(_never: never) {
-    throw new Error("Invalid operation type");
-  }
-
   try {
-    switch (req.body.type) {
-      case "HealthCheck":
-        req.body = NewHealthCheckSchema.parse(req.body);
-        return next();
-      case "Hospital":
-        req.body = NewHospitalSchema.parse(req.body);
-        return next();
-      case "OccupationalHealthcare":
-        req.body = NewOccupationalHealthcareSchema.parse(req.body);
-        return next();
-      default:
-        return exhaustiveMatchingGuard(req.body);
-    }
+    const NewEntrySchema = z.discriminatedUnion("type", [
+      NewHospitalSchema,
+      NewHealthCheckSchema,
+      NewOccupationalHealthcareSchema
+    ]);
+
+    req.body = NewEntrySchema.parse(req.body);
+    next();
   } catch (error: unknown) {
     next(error);
   }
